26c1afa7486e7e0b64c9ea65186dfab750b4e9b2
Laura Boettcher <lboettcher1500@gmail.com>
2023-03-17 15:47:35 -0400

    Authentication part 1

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,36 +1,80 @@
 import express from 'express';
+import {
+  AuthenticationError,
+  RequestError,
+} from '../constants/commonErrors.js';
+import Prisma from '../tools/prisma.js';
+import bcrypt from 'bcrypt';
+import jwt from '../tools/jwt.js';
 
 const router = express.Router();
 
 export default router
-  // create
   .post('/signup', async (request, response, next) => {
     try {
-      // TODO: what should happen when user signs up?
-      // need email & password, maybe name?
-      // check if exists
-      // if not, create new user
-      // return new accessToken (JWT) containing their ID
-
-      response.json({
-        message: 'Change me',
+      if (!request.body.name || !request.body.email || !request.body.password) {
+        throw new RequestError(
+          'Must provide a valid name, email, and password'
+        );
+      }
+      const user = await Prisma.user.findUnique({
+        where: {
+          email: request.body.email,
+        },
       });
+      if (user) {
+        throw new RequestError('An account with that email already exists');
+      }
+      const passwordHash = await bcrypt.hash(request.body.password, 10);
+      const newUser = await Prisma.user.create({
+        data: {
+          name: request.body.name,
+          email: request.body.email.toLowerCase(),
+          password: passwordHash,
+        },
+        // TODO: ACTIVITY TOKEN
+      });
+      const accessToken = jwt.sign(
+        {
+          id: newUser.id,
+        },
+        '1w'
+      );
+      response.json(accessToken);
     } catch (error) {
       next(error);
     }
   })
+
   .post('/signin', async (request, response, next) => {
     try {
-      // TODO: what should happen when user signs in?
-      // need email and password only
-      // retrieve user from db
-      // compare the password to the hash
-
-      // to use this, import bcrypt (as seen in user route)
-      // const isAuthenticated = await bcrypt.compare(INPUT_PASSWORD, PASSWORD_HASH);
-      response.json({
-        message: 'Change me',
+      if (!request.body.name || !request.body.email || !request.body.password) {
+        throw new RequestError('Must provide a valid email and password');
+      }
+      const user = await Prisma.user.findUnique({
+        where: {
+          email: request.body.email,
+        },
       });
+      if (!user) {
+        throw new RequestError('Account does not exist');
+      }
+      const isAuthenticated = await bcrypt.compare(
+        request.body.password,
+        user.password
+      );
+      if (!isAuthenticated) {
+        throw new AuthenticationError('Cannot verify login information');
+      }
+      const accessToken = jwt.sign(
+        {
+          id: user.id,
+        },
+        '1w'
+      );
+
+      // TODO: activity token
+      response.json({ accessToken });
     } catch (error) {
       next(error);
     }
