f49e1929b754613de7796407cb4fa179433abd4a
DESKTOP-4Q1O459\katmo <katherinemolina726@gmail.com>
2023-05-31 22:04:47 -0400

    Added the following suggested changes: - moved email username, email key, and domain to .env file - made subject parameter required - added validations for params - switched from promises to async/await -added default from parameter

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,33 +1,95 @@
 import formData from 'form-data';
 import Mailgun from 'mailgun.js';
+import config from '../config.js';
+import validator from 'validator';
 
-const DOMAIN = 'api.connectorlando.tech';
+const { DOMAIN } = config;
 const mailgun = new Mailgun(formData);
 const client = mailgun.client({
-  username: 'api',
-  key: 'key-be435e340c2674abc7d433862acbe740',
+  username: config.EMAIL_USERNAME,
+  key: config.EMAIL_KEY,
 });
 
-function sendTextEmail({ from, to, subject = 'ConnectOrlando', text }) {
-  client.messages
-    .create(DOMAIN, { from, to, subject, text })
-    .then(res => {
-      console.log(res);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+async function sendTextEmail({
+  from = 'hello@connectorlando.tech',
+  to,
+  subject,
+  text,
+}) {
+  if (
+    validator.isEmail(from) &&
+    to &&
+    validator.isEmail(to) &&
+    subject &&
+    text
+  ) {
+    try {
+      await client.messages.create(DOMAIN, {
+        from,
+        to,
+        subject,
+        text,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  } else {
+    var errorString = 'emailService requires a valid: \n';
+    if (!from || !validator.isEmail(from)) {
+      errorString += '- from\n';
+    }
+    if (!to || !validator.isEmail(to)) {
+      errorString += '- to\n';
+    }
+    if (!subject) {
+      errorString += '- subject\n';
+    }
+    if (!text) {
+      errorString += '- text\n';
+    }
+    throw new Error(errorString);
+  }
 }
 
-function sendHtmlEmail({ from, to, subject = 'ConnectOrlando', html }) {
-  client.messages
-    .create(DOMAIN, { from, to, subject, html })
-    .then(res => {
-      console.log(res);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+async function sendHtmlEmail({
+  from = 'hello@connectorlando.tech',
+  to,
+  subject,
+  html,
+}) {
+  if (
+    validator.isEmail(from) &&
+    to &&
+    validator.isEmail(to) &&
+    subject &&
+    html
+  ) {
+    try {
+      await client.messages.create(DOMAIN, {
+        from,
+        to,
+        subject,
+        html,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  } else {
+    var errorString = 'emailService requires a valid: \n';
+    if (!from || !validator.isEmail(from)) {
+      errorString += '- from\n';
+    }
+    if (!to || !validator.isEmail(to)) {
+      errorString += '- to\n';
+    }
+    if (!subject) {
+      errorString += '- subject\n';
+    }
+    if (!html) {
+      errorString += '- html\n';
+    }
+    throw new Error(errorString);
+  }
 }
 
 export default {
