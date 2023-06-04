32124760038b96c59baffe1bed3342d77f825284
Laura Boettcher <lboettcher1500@gmail.com>
2023-06-04 09:46:58 -0400

    Converting all routes to use routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,81 +9,79 @@ import Prisma from '../tools/prisma.js';
 
 const router = express.Router();
 
-export default router
-  .post('/signup', async (request, response, next) => {
-    try {
-      if (!request.body.name || !request.body.email || !request.body.password) {
-        throw new RequestError(
-          'Must provide a valid name, email, and password'
-        );
-      }
-      const user = await Prisma.user.findUnique({
-        where: {
-          email: request.body.email,
-        },
-      });
-      if (user) {
-        throw new RequestError('An account with that email already exists');
-      }
-      const passwordHash = await bcrypt.hash(request.body.password, 10);
-      const newUser = await Prisma.user.create({
-        data: {
-          name: request.body.name,
-          email: request.body.email.toLowerCase(),
-          password: passwordHash,
-        },
-        // TODO: ACTIVITY TOKEN
-      });
-      const accessToken = jwt.sign(
-        {
-          id: newUser.id,
-        },
-        '1w'
-      );
-      // TODO: also, create a refreshToken and return to user
-      // first - create new database (prisma) entry
-      // then create a jwt with the user id
-      // then return the refreshToken to the user
-      response.json({ accessToken });
-    } catch (error) {
-      next(error);
+router.post('/signup', async (request, response, next) => {
+  try {
+    if (!request.body.name || !request.body.email || !request.body.password) {
+      throw new RequestError('Must provide a valid name, email, and password');
     }
-  })
-  // TODO: a new endpoint called refresh
-  // it will take in a refreshToken
-  // verify the refreshToken
-  // if it is valid, create a new accessToken
-  // return the new accessToken
-
-  .post('/signin', async (request, response, next) => {
-    try {
-      if (!request.body.name || !request.body.email || !request.body.password) {
-        throw new RequestError('Must provide a valid email and password');
-      }
-      const user = await Prisma.user.findUnique({
-        where: {
-          email: request.body.email,
-        },
-      });
-      if (!user) {
-        throw new RequestError('Account does not exist');
-      }
-      const isAuthenticated = await bcrypt.compare(
-        request.body.password,
-        user.password
-      );
-      if (!isAuthenticated) {
-        throw new AuthenticationError('Cannot verify login information');
-      }
-      const accessToken = jwt.sign(
-        {
-          id: user.id,
-        },
-        '1w'
-      );
-
-      response.json({ accessToken });
-    } catch (error) {
-      next(error);
+    const user = await Prisma.user.findUnique({
+      where: {
+        email: request.body.email,
+      },
+    });
+    if (user) {
+      throw new RequestError('An account with that email already exists');
     }
-  });
+    const passwordHash = await bcrypt.hash(request.body.password, 10);
+    const newUser = await Prisma.user.create({
+      data: {
+        name: request.body.name,
+        email: request.body.email.toLowerCase(),
+        password: passwordHash,
+      },
+      // TODO: ACTIVITY TOKEN
+    });
+    const accessToken = jwt.sign(
+      {
+        id: newUser.id,
+      },
+      '1w'
+    );
+    // TODO: also, create a refreshToken and return to user
+    // first - create new database (prisma) entry
+    // then create a jwt with the user id
+    // then return the refreshToken to the user
+    response.json({ accessToken });
+  } catch (error) {
+    next(error);
+  }
+});
+// TODO: a new endpoint called refresh
+// it will take in a refreshToken
+// verify the refreshToken
+// if it is valid, create a new accessToken
+// return the new accessToken
+
+router.post('/signin', async (request, response, next) => {
+  try {
+    if (!request.body.name || !request.body.email || !request.body.password) {
+      throw new RequestError('Must provide a valid email and password');
+    }
+    const user = await Prisma.user.findUnique({
+      where: {
+        email: request.body.email,
+      },
+    });
+    if (!user) {
+      throw new RequestError('Account does not exist');
+    }
+    const isAuthenticated = await bcrypt.compare(
+      request.body.password,
+      user.password
+    );
+    if (!isAuthenticated) {
+      throw new AuthenticationError('Cannot verify login information');
+    }
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+      },
+      '1w'
+    );
+
+    response.json({ accessToken });
+  } catch (error) {
+    next(error);
+  }
+});
+export default router;
