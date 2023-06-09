aafa4cef983e4040cc68f44c230c81f3dfe9a191
Laura Boettcher <lboettcher1500@gmail.com>
2023-06-09 14:10:14 -0400

    Making all routes use routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,151 +7,149 @@ import jwt from '../tools/jwt.js';
 import { AuthenticationError } from '../constants/commonErrors.js';
 const router = express.Router();
 
-export default router
-  // create
-  .post('/', async (request, response, next) => {
-    try {
-      if (!request.body.name || !request.body.email || !request.body.password) {
-        throw new RequestError(
-          'Must provide a valid name, email, and password'
-        );
-      }
-      const user = await Prisma.user.findUnique({
-        where: {
-          email: request.body.email,
-        },
-      });
-      if (user) {
-        throw new RequestError('Email already exists');
-      }
+export default router;
+// create
+router.post('/', async (request, response, next) => {
+  try {
+    if (!request.body.name || !request.body.email || !request.body.password) {
+      throw new RequestError('Must provide a valid name, email, and password');
+    }
+    const user = await Prisma.user.findUnique({
+      where: {
+        email: request.body.email,
+      },
+    });
+    if (user) {
+      throw new RequestError('Email already exists');
+    }
 
-      const passwordHash = await bcrypt.hash(request.body.password, 10);
-      const newUser = await Prisma.user.create({
-        data: {
-          name: request.body.name,
-          email: request.body.email.toLowerCase(),
-          password: passwordHash,
-        },
-      });
-      response.json(newUser);
-    } catch (error) {
-      next(error);
+    const passwordHash = await bcrypt.hash(request.body.password, 10);
+    const newUser = await Prisma.user.create({
+      data: {
+        name: request.body.name,
+        email: request.body.email.toLowerCase(),
+        password: passwordHash,
+      },
+    });
+    response.json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get('/', async (request, response, next) => {
+  try {
+    if (!request.headers.authorization) {
+      throw new AuthenticationError('Access token missing');
     }
-  })
-  .get('/', async (request, response, next) => {
-    try {
-      if (!request.headers.authorization) {
-        throw new AuthenticationError('Access token missing');
-      }
-      const accessToken = request.headers.authorization.split(' ')[1];
-      const payload = await jwt.verify(accessToken);
-      const userID = payload.id;
-      const user = await Prisma.user.findUnique({
-        where: {
-          id: userID,
-        },
-      });
+    const accessToken = request.headers.authorization.split(' ')[1];
+    const payload = await jwt.verify(accessToken);
+    const userID = payload.id;
+    const user = await Prisma.user.findUnique({
+      where: {
+        id: userID,
+      },
+    });
 
-      const trimmedUser = _.pick(user, [
-        'id',
-        'name',
-        'email',
-        'isEmailVerified',
-        'bio',
-        'profileImage',
-      ]);
-      response.json({ user: trimmedUser });
-    } catch (error) {
-      next(error);
+    const trimmedUser = _.pick(user, [
+      'id',
+      'name',
+      'email',
+      'isEmailVerified',
+      'bio',
+      'profileImage',
+    ]);
+    response.json({ user: trimmedUser });
+  } catch (error) {
+    next(error);
+  }
+});
+// read
+router.get('/:id', async (request, response, next) => {
+  try {
+    if (!request.params.id) {
+      throw new RequestError('Must provide a valid id');
     }
-  })
-  // read
-  .get('/:id', async (request, response, next) => {
-    try {
-      if (!request.params.id) {
-        throw new RequestError('Must provide a valid id');
-      }
-      const user = await Prisma.user.findUnique({
-        where: {
-          id: request.params.id,
-        },
-      });
-      if (!user) {
-        throw new RequestError('User not found');
-      }
-      if (user.isArchived) {
-        throw new ArchivedError('Account already deleted');
-      }
+    const user = await Prisma.user.findUnique({
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (!user) {
+      throw new RequestError('User not found');
+    }
+    if (user.isArchived) {
+      throw new ArchivedError('Account already deleted');
+    }
 
-      const trimmedUser = _.pick(user, ['name', 'email', 'profile']);
-      response.json({
-        user: trimmedUser,
-      });
-    } catch (error) {
-      next(error);
+    const trimmedUser = _.pick(user, ['name', 'email', 'profile']);
+    response.json({
+      user: trimmedUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//update
+router.patch('/:id', async (request, response, next) => {
+  try {
+    if (!request.params.id) {
+      throw new RequestError('Must provide a valid id');
     }
-  })
-  //update
-  .patch('/:id', async (request, response, next) => {
-    try {
-      if (!request.params.id) {
-        throw new RequestError('Must provide a valid id');
-      }
 
-      const dataToUpdate = _.pick(request.body, [
-        'name',
-        'profileImage',
-        'title',
-        'isInvestor',
-        'linkedin',
-      ]); //etc, nothing compromising!
-      await Prisma.user.update({
-        where: {
-          id: request.params.id,
-        },
-        data: dataToUpdate,
-      });
-      response.json({
-        message: 'Succesfully updated user',
-      });
-    } catch (error) {
-      next(error);
+    const dataToUpdate = _.pick(request.body, [
+      'name',
+      'profileImage',
+      'title',
+      'isInvestor',
+      'linkedin',
+    ]); //etc, nothing compromising!
+    await Prisma.user.update({
+      where: {
+        id: request.params.id,
+      },
+      data: dataToUpdate,
+    });
+    response.json({
+      message: 'Succesfully updated user',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//delete
+router.delete('/:id', async (request, response, next) => {
+  try {
+    if (!request.params.id) {
+      throw new RequestError('Must provide a valid id');
     }
-  })
-  //delete
-  .delete('/:id', async (request, response, next) => {
-    try {
-      if (!request.params.id) {
-        throw new RequestError('Must provide a valid id');
-      }
-      // non blocking - send notes about async/await
-      await Prisma.user.update({
-        where: {
-          id: request.params.id,
-        },
-        data: {
-          isArchived: true,
-          archivedAt: new Date(),
-        },
-      });
-      response.json({
-        message: 'Successfully archived user',
-      });
-    } catch (error) {
-      next(error);
-    }
-  })
-  .get('/user', async (request, response, next) => {
-    try {
-      const token = request.headers.authorization.split(' ')[1];
-      const decodedToken = jwt.verify(token);
-      const user = await Prisma.user.findUnique({
-        where: {
-          id: decodedToken.id,
-        },
-      });
-      response.json(user);
-    } catch (error) {
-      next(error);
-    }
-  });
+    // non blocking - send notes about async/await
+    await Prisma.user.update({
+      where: {
+        id: request.params.id,
+      },
+      data: {
+        isArchived: true,
+        archivedAt: new Date(),
+      },
+    });
+    response.json({
+      message: 'Successfully archived user',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+router.get('/user', async (request, response, next) => {
+  try {
+    const token = request.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token);
+    const user = await Prisma.user.findUnique({
+      where: {
+        id: decodedToken.id,
+      },
+    });
+    response.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
