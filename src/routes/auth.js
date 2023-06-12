9b644ed09c8408f0c0ad13384dc6f4b0a1e0a64c
Laurabtt152 <125603107+Laurabtt152@users.noreply.github.com>
2023-06-11 20:51:41 -0400

    Making all routes use routes (#18)
    * Making all routes use routes
    
    * Added line breaks and edited buisness.js file

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -11,127 +11,129 @@ import Logger from '../tools/logger.js';
 
 const router = express.Router();
 
-export default router
-  .post('/signup', async (request, response, next) => {
-    try {
-      if (!request.body.name || !request.body.email || !request.body.password) {
-        throw new RequestError(
-          'Must provide a valid name, email, and password'
-        );
-      }
-      const user = await Prisma.user.findUnique({
+router.post('/signup', async (request, response, next) => {
+  try {
+    if (!request.body.name || !request.body.email || !request.body.password) {
+      throw new RequestError('Must provide a valid name, email, and password');
+    }
+    const user = await Prisma.user.findUnique({
+      where: {
+        email: request.body.email,
+      },
+    });
+    if (user) {
+      throw new RequestError('An account with that email already exists');
+    }
+    const passwordHash = await bcrypt.hash(request.body.password, 10);
+    const newUser = await Prisma.user.create({
+      data: {
+        name: request.body.name,
+        email: request.body.email.toLowerCase(),
+        password: passwordHash,
+      },
+      // TODO: ACTIVITY TOKEN
+    });
+    const accessToken = jwt.sign(
+      {
+        id: newUser.id,
+      },
+      '1w'
+    );
+
+    const refreshToken = await TokenService.getSignedRefreshToken({
+      request,
+      userId: newUser.id,
+    });
+    response.json({ accessToken, refreshToken });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/signin', async (request, response, next) => {
+  try {
+    if (!request.body.email || !request.body.password) {
+      throw new RequestError('Must provide a valid email and password');
+    }
+    const user = await Prisma.user.findUnique({
+      where: {
+        email: request.body.email,
+      },
+    });
+    if (!user) {
+      throw new RequestError('Account does not exist');
+    }
+    const isAuthenticated = await bcrypt.compare(
+      request.body.password,
+      user.password
+    );
+    if (!isAuthenticated) {
+      throw new AuthenticationError('Cannot verify login information');
+    }
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+      },
+      '1w'
+    );
+
+    const refreshToken = await TokenService.getSignedRefreshToken({
+      request,
+      userId: user.id,
+    });
+
+    response.json({ accessToken, refreshToken });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/signout', async (request, response) => {
+  try {
+    if (request.body.refreshToken) {
+      const refreshTokenInfo = await jwt.verify(request.body.refreshToken);
+      await Prisma.refreshTokens.delete({
         where: {
-          email: request.body.email,
+          id: refreshTokenInfo?.refreshTokenId,
         },
       });
-      if (user) {
-        throw new RequestError('An account with that email already exists');
-      }
-      const passwordHash = await bcrypt.hash(request.body.password, 10);
-      const newUser = await Prisma.user.create({
-        data: {
-          name: request.body.name,
-          email: request.body.email.toLowerCase(),
-          password: passwordHash,
-        },
-        // TODO: ACTIVITY TOKEN
-      });
-      const accessToken = jwt.sign(
-        {
-          id: newUser.id,
-        },
-        '1w'
-      );
-
-      const refreshToken = await TokenService.getSignedRefreshToken({
-        request,
-        userId: newUser.id,
-      });
-      response.json({ accessToken, refreshToken });
-    } catch (error) {
-      next(error);
     }
-  })
-  .post('/signin', async (request, response, next) => {
-    try {
-      if (!request.body.email || !request.body.password) {
-        throw new RequestError('Must provide a valid email and password');
-      }
-      const user = await Prisma.user.findUnique({
-        where: {
-          email: request.body.email,
-        },
-      });
-      if (!user) {
-        throw new RequestError('Account does not exist');
-      }
-      const isAuthenticated = await bcrypt.compare(
-        request.body.password,
-        user.password
-      );
-      if (!isAuthenticated) {
-        throw new AuthenticationError('Cannot verify login information');
-      }
-      const accessToken = jwt.sign(
-        {
-          id: user.id,
-        },
-        '1w'
-      );
+  } catch (error) {
+    Logger.info(
+      `Possible that refreshToken did not exist and could not be deleted. \n Error Message: ${error.message}`
+    );
+  } finally {
+    response.json({ message: 'Successfully logged out' });
+  }
+});
 
-      const refreshToken = await TokenService.getSignedRefreshToken({
-        request,
-        userId: user.id,
-      });
+router.post('/refresh', async (request, res, next) => {
+  try {
+    const refreshToken = await TokenService.extractRefreshToken(
+      request.body.refreshToken
+    );
+    const user = await Prisma.user.findUnique({
+      where: { id: refreshToken.userId },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+    if (!user) {
+      throw new AuthenticationError('Invalid refresh token');
+    }
 
-      response.json({ accessToken, refreshToken });
-    } catch (error) {
-      next(error);
-    }
-  })
-  .post('/signout', async (request, response) => {
-    try {
-      if (request.body.refreshToken) {
-        const refreshTokenInfo = await jwt.verify(request.body.refreshToken);
-        await Prisma.refreshTokens.delete({
-          where: {
-            id: refreshTokenInfo?.refreshTokenId,
-          },
-        });
-      }
-    } catch (error) {
-      Logger.info(
-        `Possible that refreshToken did not exist and could not be deleted. \n Error Message: ${error.message}`
-      );
-    } finally {
-      response.json({ message: 'Successfully logged out' });
-    }
-  })
-  .post('/refresh', async (request, res, next) => {
-    try {
-      const refreshToken = await TokenService.extractRefreshToken(
-        request.body.refreshToken
-      );
-      const user = await Prisma.user.findUnique({
-        where: { id: refreshToken.userId },
-        select: {
-          id: true,
-          email: true,
-          name: true,
-        },
-      });
-      if (!user) {
-        throw new AuthenticationError('Invalid refresh token');
-      }
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+      },
+      '15min'
+    );
+    res.json({ accessToken });
+  } catch (error) {
+    next(error);
+  }
+});
 
-      const accessToken = jwt.sign(
-        {
-          id: user.id,
-        },
-        '15min'
-      );
-      res.json({ accessToken });
-    } catch (error) {
-      next(error);
-    }
-  });
+export default router;
--- src/routes/business.js
+++ src/routes/business.js
@@ -6,107 +6,106 @@ import jwt from '../tools/jwt.js';
 import { AuthenticationError } from '../constants/commonErrors.js';
 const router = express.Router();
 
-export default router
-  .get('/:id', async (request, response, next) => {
-    try {
-      if (!request.params.id) {
-        throw new RequestError('Must provide a valid id');
-      }
-      const business = await Prisma.business.findUnique({
-        where: {
-          id: request.params.id,
-        },
-      });
-      if (!business) {
-        throw new RequestError(
-          `Could not find business with id ${request.params.id}`
-        );
-      }
-      if (business.isArchived) {
-        throw new ArchivedError('Business account already deleted');
-      }
-
-      _.pick(business, ['name', 'email', 'profile']); //etc
-      response.json(business);
-    } catch (error) {
-      next(error);
+router.get('/:id', async (request, response, next) => {
+  try {
+    if (!request.params.id) {
+      throw new RequestError('Must provide a valid id');
     }
-  })
-
-  // create
-  .post('/', async (request, response, next) => {
-    try {
-      const { name, type, mission } = request.body;
-
-      if (!name || !type || !mission) {
-        throw new RequestError('Must provide a valid name, type, and mission');
-      }
-
-      const newBusiness = await Prisma.business.create({
-        data: {
-          name,
-          type,
-          mission,
-        },
-        select: {
-          name: true,
-          type: true,
-          mission: true,
-        },
-      });
-
-      response.json(newBusiness);
-    } catch (error) {
-      next(error);
+    const business = await Prisma.business.findUnique({
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (!business) {
+      throw new RequestError(
+        `Could not find business with id ${request.params.id}`
+      );
     }
-  })
+    if (business.isArchived) {
+      throw new ArchivedError('Business account already deleted');
+    }
 
-  //update
-  .patch('/:id', async (request, response, next) => {
-    try {
-      if (!request.params.id) {
-        throw new RequestError('Must provide a valid id');
-      }
-      const cleanbody = _.pick(request.body, ['name', 'type', 'mission', '']);
-      if (_.isEmpty(cleanbody)) {
-        throw new RequestError('Nothing to update');
-      }
-      await Prisma.business.update({
-        where: {
-          id: request.params.id,
-        },
-        data: cleanbody,
-      });
-      response.json({
-        message: 'Succesfully updated business',
-      });
-    } catch (error) {
-      next(error);
+    _.pick(business, ['name', 'email', 'profile']); //etc
+    response.json(business);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// create
+router.post('/', async (request, response, next) => {
+  try {
+    const { name, type, mission } = request.body;
+
+    if (!name || !type || !mission) {
+      throw new RequestError('Must provide a valid name, type, and mission');
     }
-  })
-  //delete
-  .delete('/:id', async (request, response, next) => {
-    console.log('Here');
-    try {
-      if (!request.params.id) {
-        throw new RequestError('Must provide a valid id');
-      }
-      await Prisma.business.update({
-        where: {
-          id: request.params.id,
-        },
-        data: {
-          isArchived: true,
-          archivedAt: new Date(),
-        },
-      });
-      response.json({
-        message: 'Successfully archived business',
-      });
-    } catch {
-      next();
+
+    const newBusiness = await Prisma.business.create({
+      data: {
+        name,
+        type,
+        mission,
+      },
+      select: {
+        name: true,
+        type: true,
+        mission: true,
+      },
+    });
+
+    response.json(newBusiness);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//update
+router.patch('/:id', async (request, response, next) => {
+  try {
+    if (!request.params.id) {
+      throw new RequestError('Must provide a valid id');
     }
-  });
+    const cleanbody = _.pick(request.body, ['name', 'type', 'mission', '']);
+    if (_.isEmpty(cleanbody)) {
+      throw new RequestError('Nothing to update');
+    }
+    await Prisma.business.update({
+      where: {
+        id: request.params.id,
+      },
+      data: cleanbody,
+    });
+    response.json({
+      message: 'Succesfully updated business',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//delete
+router.delete('/:id', async (request, response, next) => {
+  console.log('Here');
+  try {
+    if (!request.params.id) {
+      throw new RequestError('Must provide a valid id');
+    }
+    await Prisma.business.update({
+      where: {
+        id: request.params.id,
+      },
+      data: {
+        isArchived: true,
+        archivedAt: new Date(),
+      },
+    });
+    response.json({
+      message: 'Successfully archived business',
+    });
+  } catch {
+    next();
+  }
+});
 
 router.get('/favorites', async (request, response, next) => {
   try {
@@ -118,7 +117,7 @@ router.get('/favorites', async (request, response, next) => {
     const userID = payload.id;
     const user = await Prisma.user.findUnique({
       where: {
-        id: userID,
+        id: request.authorizedUser.id,
       },
       include: {
         favorites: {
@@ -164,6 +163,7 @@ router.get('/connected', async (request, response, next) => {
     next(error);
   }
 });
+
 // Add a business to user's favorites
 router.post('/favorites/:id', async (request, response, next) => {
   try {
@@ -248,3 +248,4 @@ router.post('/connections/:id', async (request, response, next) => {
     next(error);
   }
 });
+export default router;
