c6eda965112790f850c3e7d413d1f5e3bc1aca47
Pablo Najera <pablo@najera.cc>
2023-08-25 12:56:33 -0600

    fixing test

--- tests/routes/auth.test.js
+++ tests/routes/auth.test.js
@@ -7,10 +7,17 @@ import emailService from '../../src/services/emailService.js';
 
 const request = supertest(app);
 
-jest.mock('../../src/services/emailService.js', () => {
-  return {
-    sendHtmlEmail: jest.fn(() => true),
-  };
+beforeEach(() => {
+  // this mocks the sendHtmlEmail function so that it does not actually send an email
+  emailService.sendHtmlEmail = jest.fn(() => true);
+});
+
+afterEach(() => {
+  // this clears the mock after each test so that it does not interfere with other tests
+  // for example, if you have a test that checks how many times a function was called
+  // and you don't clear the mock, it will interfere with the next test
+  // because the mock will still have the number of times it was called from the previous test
+  jest.clearAllMocks();
 });
 
 describe('Auth Routes', () => {
@@ -25,6 +32,14 @@ describe('Auth Routes', () => {
       expect(typeof response.body.accessToken).toBe('string');
       expect(typeof response.body.refreshToken).toBe('string');
       expect(emailService.sendHtmlEmail).toHaveBeenCalled();
+      // This checks that the function was called with an object containing the following properties
+      expect(emailService.sendHtmlEmail).toHaveBeenCalledWith(
+        expect.objectContaining({
+          email: 'test@example.com',
+          x: expect.any(Number),
+          y: expect.any(String),
+        })
+      );
     });
 
     it('should require a name', async () => {
